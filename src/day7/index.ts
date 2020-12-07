ae77c804e18c18d109ee977ef2152195e2a96e35
Miles Bardon <milesbardon@gmail.com>
2020-12-07 17:41:21 +0000

    Simplifying code

--- src/day7/index.ts
+++ src/day7/index.ts
@@ -1,5 +1,7 @@
 type BagContents = { colour: string; count: number }[];
 
+const ownBagColour = 'shiny gold';
+
 const parseInput = (input: string[]) => {
   const output: Record<string, BagContents> = {};
 
@@ -24,75 +26,74 @@ const parseInput = (input: string[]) => {
   return output;
 };
 
-const findBagWithColour = (
+const countBagsWithColour = (
   colour: string,
   globalInput: Record<string, BagContents>,
   listOfBags: string[],
   total: number
-) => {
-  let runningTotal = total;
+): number =>
+  listOfBags.reduce((runningTotal, bagColour) => {
+    // If the colour we're looking for is inside the bag we're currently
+    // on, we just up the count found in this run. Otherwise, we keep
+    // going deeper into the bags and retain that total.
+    return globalInput[bagColour].some((contents) => contents.colour === colour)
+      ? runningTotal + 1
+      : countBagsWithColour(
+          colour,
+          globalInput,
+          globalInput[bagColour].map(({ colour }) => colour),
+          runningTotal
+        );
+  }, total);
 
-  listOfBags.forEach((bagColour) => {
-    if (globalInput[bagColour].some((contents) => contents.colour === colour)) {
-      runningTotal++;
-    } else {
-      runningTotal = findBagWithColour(
-        colour,
-        globalInput,
-        globalInput[bagColour].map(({ colour }) => colour),
-        runningTotal
-      );
-    }
-  });
-
-  return runningTotal;
-};
+const countContainedBags = (
+  colour: string,
+  globalInput: Record<string, BagContents>,
+  total: number
+): number =>
+  // We need to count the contained bags, including the one that's currently being counted.
+  globalInput[colour].reduce(
+    (runningTotal, contents) =>
+      runningTotal +
+      contents.count * countContainedBags(contents.colour, globalInput, 1),
+    total
+  );
 
 const part1 = (input: string[]) => {
   const globalInput = parseInput(input);
   const listOfBags = Object.keys(globalInput);
-  const result: Record<string, number> = {};
 
-  const ownBagColour = 'shiny gold';
-
-  listOfBags.forEach((colour) => {
-    result[colour] = globalInput[colour].some(
-      (contents) => contents.colour === ownBagColour
-    )
-      ? 1
-      : findBagWithColour(
-          ownBagColour,
-          globalInput,
-          globalInput[colour].map(({ colour }) => colour),
-          0
-        );
-  });
-
-  return Object.values(result).filter((value) => value).length;
-};
-
-const countBags = (
-  colour: string,
-  globalInput: Record<string, BagContents>,
-  total: number
-) => {
-  let runningTotal = total;
-
-  globalInput[colour].forEach((contents) => {
-    runningTotal += contents.count * countBags(contents.colour, globalInput, 1);
-  });
-
-  return runningTotal;
+  // Loop through the top level of the global input, which
+  // can only contain unique values. We look in each top
+  // level bag to see if that contains our bag, otherwise
+  // invoke the recursion.
+  return (
+    listOfBags
+      .map((colour) =>
+        globalInput[colour].some((contents) => contents.colour === ownBagColour)
+          ? 1
+          : countBagsWithColour(
+              ownBagColour,
+              globalInput,
+              globalInput[colour].map(({ colour }) => colour),
+              0
+            )
+      )
+      // We just need to know how many values above 0 there are.
+      .filter((v) => v).length
+  );
 };
 
 const part2 = (input: string[]) => {
   const globalInput = parseInput(input);
 
-  return globalInput['shiny gold'].reduce(
+  // We simply need to start counting the number of bags contained within
+  // our own bag. The recursion will handle the rest, but need to start
+  // with the bag itself first (the '1' as a starting value).
+  return globalInput[ownBagColour].reduce(
     (count, contents) =>
       count +
-      contents.count * countBags(contents.colour, globalInput, 0) +
-      contents.count,
+      contents.count * countContainedBags(contents.colour, globalInput, 1),
     0
   );
 };
